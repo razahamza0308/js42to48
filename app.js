0ecdb9037616e7cba3ed4f98a8e4a9d7bb630824
razahamza0308 <razahamza0308@gmail.com>
2021-06-09 22:00:26 +0500

    sd

---
+++ app.js
@@ -0,0 +1,15 @@
+function alerts(){
+    alert("Welcome User");
+}
+function img1() {
+    alert("Model: r3 \n Company: Huawei \n Price: 18,000 PKR")
+}
+function img2() {
+    alert("Model: X \n Company: Iphone \n Price: 80,999 PKR");
+}
+function img3() {
+    alert("Model: P3 \n Company: oppo \n Price: 17,000 PKR");
+}
+
+
+
